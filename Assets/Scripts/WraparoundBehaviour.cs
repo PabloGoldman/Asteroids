f6b369638e2ad0078c6d97de245bd0e9ad6be2f6
PabloGoldman <pablo.goldmanarca@gmail.com>
2023-04-03 20:23:07 -0300

    Fixed wraparound bug in asteroids

--- Assets/Scripts/WraparoundBehaviour.cs
+++ Assets/Scripts/WraparoundBehaviour.cs
@@ -7,13 +7,32 @@ public class WraparoundBehaviour : MonoBehaviour
     private float screenWidth;
     private float screenHeight;
 
-    void Start()
+    bool hasEnteredScreen;
+
+    Camera mainCamera;
+
+    private void Awake()
     {
-        screenWidth = Camera.main.orthographicSize * 2f * Screen.width / Screen.height;
-        screenHeight = Camera.main.orthographicSize * 2f;
+        mainCamera = Camera.main;
     }
 
-    void Update()
+    private void Start()
+    {
+        screenWidth = mainCamera.orthographicSize * 2f * Screen.width / Screen.height;
+        screenHeight = mainCamera.orthographicSize * 2f;
+    }
+
+    private void CheckIfHasEnteredScreen()
+    {
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+        if (viewportPosition.x >= 0 && viewportPosition.x <= 1 && viewportPosition.y >= 0 && viewportPosition.y <= 1)
+        {
+            hasEnteredScreen = true;
+        }
+    }
+
+    private void CheckWraparound()
     {
         if (transform.position.x < -screenWidth / 2f)
         {
@@ -31,6 +50,18 @@ public class WraparoundBehaviour : MonoBehaviour
         else if (transform.position.y > screenHeight / 2f)
         {
             transform.position = new Vector2(transform.position.x, transform.position.y - screenHeight);
+        }
+    }
+
+    private void Update()
+    {
+        if (!hasEnteredScreen)
+        {
+            CheckIfHasEnteredScreen();
+        }
+        else
+        {
+            CheckWraparound();
         }
     }
 }
