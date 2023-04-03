a9015023bad90be4a65ee448e3cd2abe792d6afd
PabloGoldman <pablo.goldmanarca@gmail.com>
2023-04-03 14:15:17 -0300

    Updated bullet to ignore player collision

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,8 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] float speed = 100.0f;
 
+    float lifeTime = 5.0f;
+
     Rigidbody2D rb;
 
     private void Awake()
@@ -16,5 +18,12 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         rb.AddForce(transform.up * speed);
+
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
     }
 }
